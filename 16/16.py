3027b8b25ff2577b51dbb8524eee7e662c9ecf2d
Gigi <gigi.sayfan@aclimalabs.com>
2015-08-16 15:20:35 -0700

    Python one-liner solution to problem 16 (trivial)

---
+++ 16/16.py
@@ -0,0 +1,8 @@
+"""Power Digit Sum
+
+215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 21000?
+"""
+assert sum(int(x) for x in str(2 ** 1000)) == 1366
+
