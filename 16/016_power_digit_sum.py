c51b741f81f7d020263f091b81a15d5f13c8a696
gigi.sayfan <the.gigi@gmail.com>
2017-11-04 19:48:00 -0700

    Add link to problem

--- 16/016_power_digit_sum.py
+++ 16/016_power_digit_sum.py
@@ -2,7 +2,7 @@
 
 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-What is the sum of the digits of the number 21000?
+What is the sum of the digits of the number 2^1000?
 """
 
 assert sum(int(x) for x in str(2 ** 1000)) == 1366
